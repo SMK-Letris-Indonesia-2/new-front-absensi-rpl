1003006a1c00a3deb218ede8f67ecc3e0cffa9f9
fahkrieal <m.fahkri123@gmail.com>
2022-12-03 11:30:32 +0700

    tambah control loginasguru

---
+++ src/com/controller/controller_loginasgr.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.controller;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Lab 1
+ */
+public interface controller_loginasgr {
+    public void Setujui(loginasguru loginasgr) throws SQLException;
+    public void Tambah (loginasguru loginasgr) throws SQLException;
+}
