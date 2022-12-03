8f62a72740df5f0f5885b2a8128e2d5239eddf84
DipoLetris <dipoarya420@gmail.com>
2022-12-03 11:13:16 +0700

    menambahkan controller murid

---
+++ src/com/controller/controller_loginmurid.java
@@ -0,0 +1,17 @@
+
+import com.view.loginFrame;
+import java.sql.SQLException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LAB 2 PC 36
+ */
+public interface controller_loginmurid {
+    public void simpan (loginFrame lf) throws SQLException;   
+}
