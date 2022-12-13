6c4aab7f9861b60b60805199acd404f78d285d6d
Lotsoo <65107724+Lotsoo@users.noreply.github.com>
2022-12-13 18:06:43 +0700

    Update model_loginmurid.java
    Merapikan kode, serta melakukan metode DRY pada import java.sql*;

--- src/com/model/model_loginmurid.java
+++ src/com/model/model_loginmurid.java
@@ -3,22 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.model;
+
 import com.controller.controller_loginmurid;
 import com.koneksi.koneksi;
 import com.view.loginFrame;
 import com.view.loginMurid;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import javax.swing.JOptionPane;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+
 /**
  *
  * @author MSI
  */
-public class model_loginmurid implements controller_loginmurid{
+public class model_loginmurid implements controller_loginmurid {
 
     @Override
     public void simpan(loginMurid lf) throws SQLException {
@@ -26,19 +23,19 @@ public class model_loginmurid implements controller_loginmurid{
         String sql = "SELECT * FROM data_murid WHERE nis=? and nama=?";
         PreparedStatement pst = con.prepareStatement(sql);
         ResultSet rs = pst.executeQuery();
-try{
-pst=con.prepareStatement(sql);
-pst.setString(1, lf.TxtNis.getText());
-pst.setString(2, lf.TxtNama.getText());
-rs=pst.executeQuery();
-if(rs.next()){
-JOptionPane.showMessageDialog(null, "Berhasil login");
-}else{
-JOptionPane.showMessageDialog(null, "Salah NIK atau Nama siswa");
-}
-}catch (SQLException e){
-    System.out.println("Error"+e);
-}
+        try {
+            pst = con.prepareStatement(sql);
+            pst.setString(1, lf.TxtNis.getText());
+            pst.setString(2, lf.TxtNama.getText());
+            rs = pst.executeQuery();
+            if (rs.next()) {
+                JOptionPane.showMessageDialog(null, "Berhasil login");
+            } else {
+                JOptionPane.showMessageDialog(null, "Salah NIK atau Nama siswa");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error" + e);
+        }
     }
-    
+
 }
